9e7fff0d806d4fdb8d69ecccd046d897c0d6195f
hejiafu <hejiafu@secoo.com>
2015-12-08 17:42:49 +0800

    add consumer

--- dubbo-demo-provider/src/main/java/study/learn/dubbo/provider/DemoServiceImpl.java
+++ dubbo-demo-provider/src/main/java/study/learn/dubbo/provider/DemoServiceImpl.java
@@ -1,7 +1,10 @@
 package study.learn.dubbo.provider;
 
+import org.springframework.stereotype.Service;
+
 import study.learn.dubbo.api.DemoService;
 
+@Service
 public class DemoServiceImpl implements DemoService {
 
 	public String sayString(String str) {
