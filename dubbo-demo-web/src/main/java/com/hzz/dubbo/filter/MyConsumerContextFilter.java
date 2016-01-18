605bb8a97b61db186e3dd60dfad2b8ac7ecae287
hehuazheng <avaforreg@163.com>
2016-01-18 22:32:27 +0800

    自定义注入参数

--- dubbo-demo-web/src/main/java/com/hzz/dubbo/filter/MyConsumerContextFilter.java
+++ dubbo-demo-web/src/main/java/com/hzz/dubbo/filter/MyConsumerContextFilter.java
@@ -21,7 +21,7 @@ public class MyConsumerContextFilter implements Filter {
 	@Override
 	public Result invoke(Invoker<?> invoker, Invocation invocation)
 			throws RpcException {
-		RpcContext.getContext().setAttachment("appName", "testApp");
+		RpcContext.getContext().setAttachment("appName", appName);
 		return invoker.invoke(invocation);
 	}
 
